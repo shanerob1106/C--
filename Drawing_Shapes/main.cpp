7d89588037870e2707d698c323dd2b4996443f4e
shanerob1106 <shanerob11062@gmail.com>
2024-01-19 17:23:24 +0000

    added a character movement and jump physics

--- Drawing_Shapes/main.cpp
+++ Drawing_Shapes/main.cpp
@@ -31,51 +31,34 @@ void setupCallbacks()
     }
 }
 
-bool isJumping = false;
-int jumpHeight = 50;
-int jumpSpeed = 5;
-
-void person(int x, int& y)
+void floor(int x, int y)
 {
-    static int gravity = 4; // Define the gravity value
-
-    if (isJumping)
-    {
-        y -= jumpSpeed; // Move the person upwards
-        jumpHeight -= jumpSpeed; // Decrease the jump height
-
-        if (jumpHeight <= 0)
-        {
-            isJumping = false; // Stop jumping when the jump height reaches 0
-        }
-    }
-    else
-    {
-        y += gravity; // Apply gravity to the y-coordinate
-    }
-
-    GFX::drawRect(x + 10, y + 10, 20, 20, 0xFF0000); // Draw the head
+    GFX::drawRect(x, y, 480, 20, 0x6AA84F);
 }
 
-auto main() -> int
+class Player
 {
-    setupCallbacks();
-    sceCtrlGetSamplingCycle(0);
-    sceCtrlSetSamplingMode(PSP_CTRL_MODE_ANALOG);
-    SceCtrlData pad;
-    GFX::init();
+public:
+    Player(int x, int y) : x(x), y(y), isJumping(false), jumpPressed(false), jumpHeight(40), jumpSpeed(6), gravity(4) {}
 
-    int x = 0;
-    int y = 0;
-
-    while (1)
+    void update(const SceCtrlData &pad)
     {
-        sceCtrlReadBufferPositive(&pad, 1);
+        if (isJumping)
+        {
+            y -= jumpSpeed;          // Move the person upwards
+            jumpHeight -= jumpSpeed; // Decrease the jump height
 
-        if(y > 232){
-            y= 232;
+            if (jumpHeight <= 0)
+            {
+                isJumping = false; // Stop jumping when the jump height reaches 0
+            }
+        }
+        else
+        {
+            y += gravity; // Apply gravity to the y-coordinate
         }
 
+        // Player controller
         if (pad.Buttons & PSP_CTRL_LEFT)
         {
             x--;
@@ -84,16 +67,129 @@ auto main() -> int
         {
             x++;
         }
-        else if (pad.Buttons & PSP_CTRL_CROSS)
+        else if (pad.Buttons & PSP_CTRL_CROSS && !jumpPressed)
         {
-            isJumping = true;
+            // Jumping
+            if (!jumpPressed)
+            {
+                isJumping = true;
+                jumpHeight = 40;
+                jumpPressed = true;
+            }
+            else{
+                jumpPressed = false;
+            }
         }
 
+        // Reset jumpPressed when the cross button is released
+        if (!(pad.Buttons & PSP_CTRL_CROSS))
+        {
+            jumpPressed = false;
+        }
+    }
+
+    int getY()
+    {
+        return y;
+    }
+
+    int getX()
+    {
+        return x;
+    }
+
+    void setY(int newY);
+
+    void setX(int newX);
+
+    void draw()
+    {
+        GFX::drawRect(x + 10, y + 10, 20, 20, 0xFF0000); // Draw the head
+    }
+
+private:
+    int x;
+    int y;
+    bool isJumping;
+    bool jumpPressed;
+    int jumpHeight;
+    int jumpSpeed;
+    int gravity;
+};
+
+bool isTouchingFloor(Player player, int floorY)
+{
+    // Check if the player's y-coordinate is equal to the floor's y-coordinate
+    if (player.getY() >= floorY)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool isTouchingRoof(Player player, int roofY)
+{
+    // Check if the player's y-coordinate is equal to the roof's y-coordinate
+    if (player.getY() <= roofY)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void Player::setY(int newY)
+{
+    y = newY;
+}
+
+void Player::setX(int newX)
+{
+    x = newX;
+}
+
+auto main() -> int
+{
+    setupCallbacks();
+    sceCtrlGetSamplingCycle(0);
+    sceCtrlSetSamplingMode(PSP_CTRL_MODE_ANALOG);
+    SceCtrlData pad;
+
+    Player player(0, 0);
+    int floorY = 220;
+    GFX::init();
+
+    while (1)
+    {
+        sceCtrlReadBufferPositive(&pad, 1);
+
+        // Draw calls
         GFX::clear(0xFFFFCA82);
-        GFX::drawRect(0, 252, 480, 20, 0x6AA84F);
-        person(x, y);
+
+        // Environment (make calls between clear and swapBuffers)
+        floor(0, 252);
+
+        // Player
+        player.update(pad);
+        player.draw();
+
+        if (isTouchingFloor(player, floorY))
+        {
+            player.setY(floorY);
+        }
+
+        if (isTouchingRoof(player, 0))
+        {
+            player.setY(0);
+        }
+
+        // Swap buffers
         GFX::swapBuffers();
-        
         sceDisplayWaitVblankStart();
     }
 }
